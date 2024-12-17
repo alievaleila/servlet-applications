6f4344ae0aabe1ef492fc2441f7e71139308b810
alievaleila <meliyeva452@gmail.com>
2024-12-17 21:38:11 +0400

    initial commit

---
+++ src/main/java/InfoServlet.java
@@ -0,0 +1,20 @@
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class InfoServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.setCharacterEncoding("UTF-16");
+
+        String name = req.getParameter("name");
+        String msg = String.format("Hello %s", name != null ? name : "World");
+
+        resp.getWriter().print(msg);
+
+
+    }
+
+
+}
---
+++ src/main/java/ServletApp.java
@@ -0,0 +1,22 @@
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+
+public class ServletApp {
+
+    public static void main(String[] args) throws Exception {
+        final Server server = new Server(8080);
+
+        final ServletContextHandler handler = new ServletContextHandler();
+
+        handler.addServlet(new ServletHolder(new InfoServlet()), "/info");
+        handler.addServlet(new ServletHolder(new CalcServlet()), "/calc");
+
+        server.setHandler(handler);
+
+        server.start();
+        server.join();
+
+
+    }
+}
