8b9605cdca15c4ab1f7ae3c596a32fe24835e789
alievaleila <meliyeva452@gmail.com>
2024-12-20 14:21:48 +0400

    changes

--- src/main/java/InfoServlet.java
+++ src/main/java/InfoServlet.java
@@ -1,7 +1,9 @@
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Arrays;
 
 public class InfoServlet extends HttpServlet {
     @Override
@@ -9,8 +11,15 @@ public class InfoServlet extends HttpServlet {
         resp.setCharacterEncoding("UTF-16");
 
         String name = req.getParameter("name");
+        System.err.println(">>>"+name);
+        String name2=req.getParameter("name");
+        System.err.println(">>>"+name2);
+        req.getParameterMap().forEach((key,value)->System.out.println(key+" "+ Arrays.toString(value)));
         String msg = String.format("Hello %s", name != null ? name : "World");
-
         resp.getWriter().print(msg);
+    }
+    @Override
+    protected void doPost(HttpServletRequest req,HttpServletResponse resp)throws ServletException,IOException{
+        resp.getWriter().print("I took");
     }
 }
